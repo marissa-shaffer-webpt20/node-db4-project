778dd823ac58b8f1703bc622e1d678aa5c8fc5b2
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-19 22:16:37 -0600

    Srarted recipe model set up

---
+++ data/models/recipes-model.js
@@ -0,0 +1,25 @@
+const db = require("../data/config")
+
+function find() {
+    return db("recipes")
+}
+
+function getShoppingList(id) {
+    return db("recipes as r")
+        .where("r.id", id)
+        .innerJoin("steps as s", "r.id", "s.recipe_id")        
+        .innerJoin("recipe_ingredients as ri", "ri.step", "s.step_number")
+        .innerJoin("ingredients as i")
+        .where("i.id", "ri.ingredient_id")
+        .select("r.id", "ri.quantity", "i.name")
+}
+
+function getInstructions(id) {
+    
+}
+
+module.exports = {
+    find,
+    getShoppingList,
+    getInstructions,
+}
