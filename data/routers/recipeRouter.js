75e63aff6e04dc2c63fc0db7e71cd8a765777258
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-19 22:17:56 -0600

    set up get recipes and get shopping list endpoints

--- data/routers/recipeRouter.js
+++ data/routers/recipeRouter.js
@@ -1,9 +1,31 @@
-  
 express = require("express")
 const db = require("../config")
 
+const recipeModel = require("../models/recipes-model")
+
 const router = express.Router()
 
+router.get("/recipes", async (req, res, next) => {
+    try {
+        const recipes = await Recipes.find()
+        res.json(recipes)
+    } catch(err) {
+        next(err)
+    }
+})
 
+router.get("/recipes/:id/shoppinglist", async (req, res, next) => {
+    try {
+        const recipes = await Recipes.getShoppingList(req.params.id)
+        if (!recipes) {
+            return res.status(404).json({
+                message: "recipe not found",
+            })
+        }
+        res.json(recipes)
+    } catch(err) {
+        next(err)
+    }
+})
 
 module.exports = router
